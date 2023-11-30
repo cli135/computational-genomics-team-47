df0800119c1ac78cea8715806c386725a639099e
cli135 <licli019@gmail.com>
2023-11-29 19:35:50 -0500

    Init

---
+++ main.py
@@ -0,0 +1,24 @@
+def split_fasta_into_pseudoreads(fasta_file):
+  """Splits the FASTA file into pseudoreads
+  
+  Keyword arguments:
+  argument -- description
+  Return: return_description
+  """
+  pass
+
+def make_kmer_index(param):
+  """Makes the k-mer index based off of the database of possible contaminants.
+  
+  Keyword arguments:
+  argument -- description
+  Return: return_description
+  """
+  
+  
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
