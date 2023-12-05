7ebc89678997f50dfab6e7b335b4b84e1705bd5b
cli135 <licli019@gmail.com>
2023-12-04 22:28:14 -0500

    update

--- taxonomy_tree.py
+++ taxonomy_tree.py
@@ -12,21 +12,25 @@ Code inspired from Jen Lu's code linked below
 https://github.com/jenniferlu717/KrakenTools/blob/master/make_ktaxonomy.py
 """
 
+# TaxaTree data structure representing nodes in taxonomy
 class TaxaTree:
     def __init__(self, tax_id, parent = None, children = [], name = "", rank):
+        # Node's own taxid, name, and rank
         self.tax_id = tax_id
-        self.parent = parent
-        self.c
         self.name = name
         self.rank = rank
-        self.parent = 
 
+        # Setting Parent
+        self.parent = parent
+        self.parentTaxId = -1
 
-        # initialized as None by default
-        self.parent = None
-        self.children = None
+        # Children
+        self.children = children
+        self.childrenTaxId = []
+
 
     def add_child(self, child_node):
+        # Two-way connection between nodes!
         child_node.parent = self
         self.children.append(child_node)
 
@@ -61,8 +65,22 @@ taxonomy_id_to_node = {}
 # https://github.com/DerrickWood/kraken/blob/master/src/krakenutil.cpp
 taxonomy_id_to_parent_id = {}
 
+# redundancy in the dictionaries let us navigate both up and down the tree
+# following forward pointers and backpointers in the n-ary tree like a deque
 
 def build_parent_map():
+  """
+  This method closely implements
+  https://github.com/DerrickWood/kraken/blob/master/src/krakenutil.cpp
+  
+  Which basically extracts the first two columns from nodes.dmp and puts them into
+  the taxonomy_id_to_parent_id
+  
+  Keyword arguments:
+  argument -- description
+  Return: return_description
+  """
+  
   file_handle = open('./taxonomy/nodes.dmp', 'r')
   # finish soon
   
