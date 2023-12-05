6be8315d13e9e129cb9d2be2ed985f0f71747213
cli135 <licli019@gmail.com>
2023-12-04 22:39:31 -0500

    update beginning jen's outline code in krakentools

--- taxonomy_tree.py
+++ taxonomy_tree.py
@@ -14,7 +14,7 @@ https://github.com/jenniferlu717/KrakenTools/blob/master/make_ktaxonomy.py
 
 # TaxaTree data structure representing nodes in taxonomy
 class TaxaTree:
-    def __init__(self, tax_id, parent = None, children = [], name = "", rank):
+    def __init__(self, tax_id, rank, parent = None, children = [], name = ""):
         # Node's own taxid, name, and rank
         self.tax_id = tax_id
         self.name = name
@@ -44,7 +44,8 @@ class TaxaTree:
         return self.tax_id
 
 # for utility, for convenience
-map_ranks_to_abbreviations = {
+ranks_to_charRank = {
+    "superkingdom": "SK",
     "kingdom": "K",
     "phylum": "P",
     "class": "C",
@@ -74,15 +75,53 @@ def build_parent_map():
   https://github.com/DerrickWood/kraken/blob/master/src/krakenutil.cpp
   
   Which basically extracts the first two columns from nodes.dmp and puts them into
-  the taxonomy_id_to_parent_id
+  the taxonomy_id_to_parent_id map.
   
   Keyword arguments:
   argument -- description
   Return: return_description
   """
   
-  file_handle = open('./taxonomy/nodes.dmp', 'r')
+  nodes_dmp_file_handle = open('./taxonomy/nodes.dmp', 'r')
   # finish soon
   
+  count_num_nodes = 0
+  # Iterating over the nodes.dmp (1st column in t)xId, 2nd column is the 1st col's parent's taxId
+  for line in nodes_dmp_file_handle:
+    # Increment total number of nodes we've seen so far
+    count_num_nodes += 1
+
+    # Spliting the line we are on
+    tokens = line.strip().split('\t|\t')
+    # print(tokens)
+
+    # Storing info in variables
+    current_taxonomy_id = tokens[0]
+    parent_taxonomy_id = tokens[1]
+    rank = tokens[2] # rank is one of "kingdom", "phylum", "class", "order", etc.
+    abbreviated_rank = ranks_to_charRank[rank] # "K", "P", "C", "O", etc.
+
+    # TODO: Is the current_node being created correctly? What redundancy can we 
+    # Create a node with the taxid and its rank
+    current_node = TaxaTree(current_taxonomy_id, abbreviated_rank)
+    current_node.parentTaxId = parent_taxonomy_id
+    
+    # nodes themselves have backpointers to their taxnonomy ids and their parent nodes
+    # so we have redundancy in links like forward pointers and backpointers
+    # in the n-ary tree structure
+
+    # This is Jen's map - maps taxonomy ids to nodes
+    taxonomy_id_to_node[current_taxonomy_id] = current_node
+
+    # This is Derrick's map - maps taxonomy ids to parent taxonomy ids
+    taxonomy_id_to_parent_id[current_taxonomy_id] = parent_taxonomy_id
+
+    
+    
 
 
+def main():
+  build_parent_map()
+
+if __name__ == "__main__":
+  main()
