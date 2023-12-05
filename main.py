044eb76679ae87c43cf59e493aab2454de0a9d5e
cli135 <licli019@gmail.com>
2023-12-04 22:08:15 -0500

    udpate

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-def split_fasta_into_pseudoreads(fasta_file):
-  """Splits the FASTA file into pseudoreads
+import os
+  
+def build_parent_map():
+  file_handle = open('nodes.dmp')
+  # finish soon
+
+def build_database():
+  """
+  Given a directory of ~20 FASTA files with genomes of common contaminants,
+  we want to traverse all kmers in the FASTA files.
+  
+  For each kmer update the kmer_to_lca dictionary, we want to set
+
+    kmer_to_lca[kmer] = lca(kmer_to_lca[kmer], curr_taxonomy_id)
+  
+  the least commmon ancestor of the current value in kmer_to_lca dictionary
+  and the 
   
   Keyword arguments:
   argument -- description
   Return: return_description
   """
+  # for each FASTA file
+  for f in os.listdir("./"):
+    # get the NCBI accession id for this FASTA file
+    pass
+    # get the taxonomy id for this FASTA file
+    pass
+    # for each distinct k-mer
+      # update the LCA dictionary with the LCA of the existing value and this value
   pass
 
-def make_kmer_index(param):
-  """Makes the k-mer index based off of the database of possible contaminants.
-  
-  Keyword arguments:
-  argument -- description
-  Return: return_description
-  """
-  
-  
+
+
 
 def main():
   pass
