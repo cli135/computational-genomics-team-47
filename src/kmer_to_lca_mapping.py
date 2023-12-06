1bcc8ae567b81060ada4e6bcb267160bd33851f7
cli135 <licli019@gmail.com>
2023-12-05 23:47:59 -0500

    Dhruv and Joanne worked on kmer_to_lca mapping, progress made

--- src/kmer_to_lca_mapping.py
+++ src/kmer_to_lca_mapping.py
@@ -1,7 +1,8 @@
 import os
 import taxonomy_tree
 from taxonomy_tree import TaxaTree
-from typing import Dict
+from typing import Dict, List, Set
+from collections import defaultdict
 
 """
 kmer_to_lca_mapping.py
@@ -49,7 +50,7 @@ Jaeyoon Wang
 
 # Step 2. After the parent map (i.e. taxonomy tree) is built in taxonomy_tree.py,
 # We will build the database with actual cross-references to kmers and lcas
-def build_database() -> Dict[str, int]:
+def build_database(file_directory: str, custom_taxonomy_ids_filename : str, k: int) -> Dict[str, int]:
   """
   Given a directory of ~20 FASTA files with genomes of common contaminants,
   we want to traverse all kmers in the FASTA files.
@@ -65,22 +66,85 @@ def build_database() -> Dict[str, int]:
 
   @return: the kmer_to_lca_mapping dictionary
   """
-  # TODO partial code below but unfinished
 
-  # for each FASTA file
-  for f in os.listdir("./"):
-    # get the NCBI accession id for this FASTA file
-    pass
-    # get the taxonomy id for this FASTA file
-    pass
-    # for each distinct k-mer
-      # update the LCA dictionary with the LCA of the existing value and this value
-  pass
+  # Dictionary mapping k-mers to their LCA taxonomy IDs
+  kmers_to_lca = {}
 
-  # TODO implement me!
-  raise NotImplementedError()
+  # Get the NCBI accession id to tax id mapping
+  ncbi_accession_id_to_tax_id_mapping = \
+    make_ncbi_accession_id_to_tax_id_mapping(
+      custom_taxonomy_ids_filename
+    )
+  
+  # File that we are searching through
+  file_count = 1
+
+  # For each FASTA file
+  for f in os.listdir(f"./{file_directory}"):
+
+    # opening the file
+    file_path = os.path.join(file_directory, f)
+
+    with open(file_path, "r") as reference_genome_assembly:
+      # reading in the first line of the file
+      first_line = reference_genome_assembly.readline()
+        
+      # get the accession id for this FASTA file
+      accession_id = first_line.split()[0][1:]
+
+      # check if the accession id is in the accesion id to tax id mapping
+      if (accession_id in ncbi_accession_id_to_tax_id_mapping.keys()):
+        # We are searching this file
+        print(f"searching file {file_count} with accession_id {str(accession_id)}")
+
+        # if it is, then get the tax id
+        tax_id = ncbi_accession_id_to_tax_id_mapping[accession_id]
+        
+        # Split the genome assembly into kmers
+        reference_genome_assembly_sequence = ""
+        for line in reference_genome_assembly:
+            if line.startswith(">"):
+                # Skip header lines in FASTA format
+                continue
+            # Add this line to the genome assembly sequence
+            sequence += line.strip()
+
+        for i in range(len(reference_genome_assembly_sequence) - k + 1):
+            kmer = sequence[i:i + k]
+
+            # If it is a kmer we haven't seen before, then set it to the tax_id corresponding
+            # to the accession id of this FASTA file
+            if (kmer not in kmers_to_lca.keys()):
+              kmers_to_lca[kmer] = [tax_id]
+            else:
+              # If it is a kmer we have seen before, then update the LCA of this kmer
+              kmers_to_lca[kmer] = lca(kmers_to_lca[kmer], tax_id)            
+      else:
+        continue
 
 
+def make_ncbi_accession_id_to_tax_id_mapping(
+    custom_taxonomy_ids_filename : str) -> List[str]:
+  """
+  Load a custom taxonomy IDs file and 
+
+  This removes the need to search the large file
+  nucl_wgs.accession2taxid.gz (~37 GB uncompressed)
+  for just the 20 genomes that we want to classify contaminants in.
+  """
+  ncbi_accession_id_to_tax_id = {}
+  # using the custom seqid2taxid instead of searching the whole 37 GB mapping at
+  # https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/
+  with open(custom_taxonomy_ids_filename, 'r') as fp:
+    # getting the second column of each line,
+    # which is the taxonomy ID of that accession ID
+    for line in fp.readlines():
+      tokens = line.strip().split()
+      ncbi_accession_id = tokens[0]
+      tax_id = tokens[1]
+      ncbi_accession_id_to_tax_id[ncbi_accession_id] = tax_id
+  # return the dictionary
+  return ncbi_accession_id_to_tax_id
 
 def lca(taxonomy_id_to_parent_id, first_taxonomy_id : int, second_taxonomy_id : int) -> int:
   """
@@ -102,8 +166,33 @@ def lca(taxonomy_id_to_parent_id, first_taxonomy_id : int, second_taxonomy_id : 
   # from the taxonomy_tree file
 
   # TODO implement me!
-  raise NotImplementedError()
-
+  # raise NotImplementedError()
+  a = first_taxonomy_id
+  b = second_taxonomy_id
+  # it seems we just return the other if one is '0'
+  # and '0' is not the root taxonomy id, '1' is the root
+  if a == '0' or b == '0':
+    return a if a else b
+  
+  path_from_a_to_root : Set[str] = set()
+  # collect taxonomy ids of the ndoes
+  # on the path from a towards root in a set
+  while a != '1':
+    path_from_a_to_root.add(a)
+    a = taxonomy_id_to_parent_id[a]
+  
+  # ok, now we track up from b leaf node
+  # and find the first point of intersection
+  # using the hashset built above
+  while b not in path_from_a_to_root:
+    b = taxonomy_id_to_parent_id[b]
+  
+  # at this point, b is the first node found
+  # that is also on the path from a to root.
+  # this means we found the lca, the earliest
+  # point of intersection of the path from a to root
+  # and the path from b to root
+  return b
 
 def lca(root_node : TaxaTree, first_node : TaxaTree, second_node : TaxaTree) -> TaxaTree:
   """
@@ -132,5 +221,13 @@ def lca(root_node : TaxaTree, first_node : TaxaTree, second_node : TaxaTree) -> 
   # in a binary tree
 
   # TODO implement me!
-  raise NotImplementedError()
+  # raise NotImplementedError()
 
+
+def main():
+  build_database("genomes-of-common-contaminants", "taxonomy/custom_taxonomy_ids.txt", 31)
+  lca(None, None, None)
+
+
+if __name__ == "__main__":
+  main()
